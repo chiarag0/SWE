3bef2dd4d637597f85db20ddfd02c6c591cb29e5
Chiara <gori.chiara@yahoo.it>
2022-05-18 14:26:34 +0200

    cambiamenti 18/05

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,43 @@ package com.company;
 
 import GestioneMagazzini.Key;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        TreeMap<Key,Integer> elementi = new TreeMap<>();
+       /*
+       TreeMap<Key,Integer> elementi = new TreeMap<>();
         Key k = new Key(8800,002);
         Key kk = new Key (5390,201);
         Key kkk = new Key (2232, 394);
-        elementi.put(k,1);
-        elementi.put(kk,1);
+        elementi.put(k,2);
+        elementi.put(kk,3);
         elementi.put(kkk,1);
+        String a = elementi.toString();
+        System.out.println(a);
+
+        for(Map.Entry<Key,Integer> entry : elementi.entrySet()) {
+            Key key = entry.getKey();
+            Integer value = entry.getValue();
 
 
-        System.out.println(elementi);
+        }
+
+        // Use iterator to display the keys and associated values
+        Set keys = elementi.keySet();
+        for (Iterator i = keys.iterator(); i.hasNext();) {
+            Key key = (Key) i.next();
+            Integer value = (Integer) elementi.get(key);
+            System.out.println(key + " = " + value);*/
+        }
 
     }
-}
+
 
 
 
