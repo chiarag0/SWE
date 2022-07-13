fe7907d7d2c3ec0e6f67717bd6f6e873e6359e50
Chiara <gori.chiara@yahoo.it>
2022-07-13 12:25:30 +0200

    inizio StockManagerTest

--- src/test/java/StockManagement/StockManagerTest.java
+++ src/test/java/StockManagement/StockManagerTest.java
@@ -1,12 +1,25 @@
 package StockManagement;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StockManagerTest {
+    StockManager sm = StockManager.getInstance();
+
+    @BeforeAll
+    public void setUp() throws Exception {
+        Fumetto f1 = new Fumetto("panini", 2, "aaa", 1);
+        Fumetto f2 = new Fumetto("panini", 3, "aaa", 2);
+        Fumetto f3 = new Fumetto("panini", 4, "aaa", 2);
+        sm.addFumetto(f1);
+        sm.addFumetto(f2);
+        sm.addFumetto(f3);
+    }
 
     @Test
     void addFumetto() {
+       assertEquals(sm.fumetti.size(),3);
     }
 }
